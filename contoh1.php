09fbfe93afdd25f21d8206f4b258ba11b683f1b7
antosony <115717775+antosony@users.noreply.github.com>
2022-10-13 22:27:12 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>perkenalkan</h1>";
+		echo"Nama saya Anto Sony Wijaya
+		saya tinggal di tegal hobi saya adalah bermusik";
+	}
+}
